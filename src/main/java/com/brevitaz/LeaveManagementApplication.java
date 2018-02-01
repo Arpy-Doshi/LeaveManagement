9064fbb0e1147c43910655c32f98fa85415dc0fd
arpy <arpydoshi28@gmail.com>
2018-02-01 11:12:43 +0530

    basiccommitdone

---
+++ src/main/java/com/brevitaz/LeaveManagementApplication.java
@@ -0,0 +1,18 @@
+package com.brevitaz;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan("com.brevitaz")
+public class LeaveManagementApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeaveManagementApplication.class, args);
+    }
+}
+
+
+
+
