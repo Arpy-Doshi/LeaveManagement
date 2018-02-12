04ff850dcc6f8b92e157f6149494bb27a96bace6
yashpatel <yashpatel2236@gmail.com>
2018-02-12 12:56:30 +0530

    all updated files are committed.

---
+++ src/main/java/com/brevitaz/controller/LeavePolicyController.java
@@ -0,0 +1,32 @@
+package com.brevitaz.controller;
+
+import com.brevitaz.model.Employee;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/leave-policy")
+public class LeavePolicyController {
+
+    @RequestMapping(value = "" , method = RequestMethod.POST)
+    public void create(@RequestBody Employee employee)
+    {
+        System.out.println("policy is created.");
+    }
+
+    @RequestMapping(value = "/{id}" , method = RequestMethod.PUT)
+    public void update(@RequestBody Employee employee, @PathVariable String id)
+    {
+        System.out.println("policy is updated.");
+    }
+    @RequestMapping(value = "/{id}" , method = RequestMethod.DELETE)
+    public void delete(@PathVariable String id)
+    {
+        System.out.println("policy is deleted.");
+    }
+
+    @RequestMapping(value = "/{id}" , method = RequestMethod.GET)
+    public void get(@PathVariable String id)
+    {
+        System.out.println("policy is viewed.");
+    }
+}
