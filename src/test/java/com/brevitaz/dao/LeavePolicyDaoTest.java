08bca388bf0f3b33b3b98949bc1fb1258e92845f
arpy <arpydoshi28@gmail.com>
2018-02-26 19:01:00 +0530

    modificn

--- src/test/java/com/brevitaz/dao/LeavePolicyDaoTest.java
+++ src/test/java/com/brevitaz/dao/LeavePolicyDaoTest.java
@@ -2,97 +2,175 @@ package com.brevitaz.dao;
 
 import com.brevitaz.model.Employee;
 import com.brevitaz.model.LeavePolicy;
+import com.brevitaz.model.LeavePolicyRule;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.expression.spel.ast.BooleanLiteral;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class LeavePolicyDaoTest {
-/*
     @Autowired
     LeavePolicyDao leavePolicyDao;
 
     @Test
-    public void createTest() throws IOException {
+    public void createTest() {
+        LeavePolicyRule leavePolicyRule = new LeavePolicyRule();
+        leavePolicyRule.setId("1");
+        leavePolicyRule.setName("Abc");
+        leavePolicyRule.setDescription("fcgtyuijklmnjbhvg");
+
+        List<LeavePolicyRule> leavePolicyRules = new ArrayList<>();
+        leavePolicyRules.add(leavePolicyRule);
+
         LeavePolicy leavePolicy = new LeavePolicy();
         leavePolicy.setId("1");
-        leavePolicy.setName("leave");
+        leavePolicy.setLeavePolicyRules(leavePolicyRules);
 
-        leavePolicy.setDescription("you have to request for leave before 15 days.");
-
-        boolean status = leavePolicyDao.create(leavePolicy);
-        Assert.assertEquals(true,status);
+        try {
+            boolean status = leavePolicyDao.create(leavePolicy);
+            Assert.assertEquals(true, status);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void getAllTest() throws IOException {
+    public void getAllTest()
+    {
+        LeavePolicyRule leavePolicyRule = new LeavePolicyRule();
+        leavePolicyRule.setId("2");
+        leavePolicyRule.setName("Abc");
+        leavePolicyRule.setDescription("fcgtyuijklmnjbhvg");
+
+        List<LeavePolicyRule> leavePolicyRules = new ArrayList<>();
+        leavePolicyRules.add(leavePolicyRule);
 
         LeavePolicy leavePolicy = new LeavePolicy();
-        leavePolicy.setId("1");
-        leavePolicy.setName("leave");
+        leavePolicy.setId("2");
+        leavePolicy.setLeavePolicyRules(leavePolicyRules);
 
-        leavePolicy.setDescription("you have to request for leave before 15 days.");
+        try {
+            boolean status = leavePolicyDao.create(leavePolicy);
+            Assert.assertEquals(true, status);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        boolean status = leavePolicyDao.create(leavePolicy);
-        Assert.assertEquals(true,status);
-
-        List<LeavePolicy> leavePolicies = leavePolicyDao.getAll();
-        Assert.assertNotNull(leavePolicies);
-
-       *//* int size = leavePolicies.size();
-        Assert.assertEquals(1,size);*//*
+        try {
+            List<LeavePolicy> leavePolicies = leavePolicyDao.getAll();
+            Assert.assertNotNull(leavePolicies);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void get() throws IOException {
+    public void updateTest()
+    {
+        LeavePolicyRule leavePolicyRule = new LeavePolicyRule();
+        leavePolicyRule.setId("4");
+        leavePolicyRule.setName("Abc");
+        leavePolicyRule.setDescription("fcgtyuijklmnjbhvg");
+
+        List<LeavePolicyRule> leavePolicyRules = new ArrayList<>();
+        leavePolicyRules.add(leavePolicyRule);
+
         LeavePolicy leavePolicy = new LeavePolicy();
-        leavePolicy.setId("1");
-        leavePolicy.setName("leave");
+        leavePolicy.setId("3");
+        leavePolicy.setLeavePolicyRules(leavePolicyRules);
 
-        leavePolicy.setDescription("you have to request for leave before 15 days.");
+        try {
+            boolean status = leavePolicyDao.create(leavePolicy);
+            Assert.assertEquals(true, status);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        boolean status = leavePolicyDao.create(leavePolicy);
-        Assert.assertEquals(true,status);
+        LeavePolicyRule leavePolicyRule1 = new LeavePolicyRule();
+        leavePolicyRule1.setId("3");
 
-        LeavePolicy leavePolicy1 = leavePolicyDao.getById("1");
-        Assert.assertNotNull(leavePolicy1);
+        List<LeavePolicyRule> leavePolicyRules1 = new ArrayList<>();
+        leavePolicyRules1.add(leavePolicyRule1);
+
+        leavePolicy.setLeavePolicyRules(leavePolicyRules1);
+
+        try {
+            boolean status1 = leavePolicyDao.update(leavePolicy,"3");
+            Assert.assertEquals(true,status1);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void update() throws IOException {
+    public void deleteTest()
+    {
+        LeavePolicyRule leavePolicyRule = new LeavePolicyRule();
+        leavePolicyRule.setId("4");
+        leavePolicyRule.setName("Abc");
+        leavePolicyRule.setDescription("fcgtyuijklmnjbhvg");
+
+        List<LeavePolicyRule> leavePolicyRules = new ArrayList<>();
+        leavePolicyRules.add(leavePolicyRule);
+
         LeavePolicy leavePolicy = new LeavePolicy();
         leavePolicy.setId("1");
-        leavePolicy.setName("leave");
+        leavePolicy.setLeavePolicyRules(leavePolicyRules);
 
-        leavePolicy.setDescription("you have to request for leave before 15 days.");
+        try {
+            boolean status = leavePolicyDao.create(leavePolicy);
+            Assert.assertEquals(true, status);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        boolean status = leavePolicyDao.create(leavePolicy);
-        Assert.assertEquals(true,status);
+        try {
+            boolean status1 = leavePolicyDao.delete("4");
+            Assert.assertEquals(true,status1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        LeavePolicy leavePolicy1 = new LeavePolicy();
-        leavePolicy.setDescription("you have to request for leave before 10 days.");
-        boolean status1 = leavePolicyDao.update(leavePolicy1,"1");
-        Assert.assertEquals(true,status1);
     }
 
     @Test
-    public void delete() throws IOException {
+    public void getByIdTest()
+    {
+        LeavePolicyRule leavePolicyRule = new LeavePolicyRule();
+        leavePolicyRule.setId("4");
+        leavePolicyRule.setName("Mno");
+        leavePolicyRule.setDescription("fcgtyuijklmnjbhvg");
+
+        List<LeavePolicyRule> leavePolicyRules = new ArrayList<>();
+        leavePolicyRules.add(leavePolicyRule);
+
         LeavePolicy leavePolicy = new LeavePolicy();
-        leavePolicy.setId("1");
-        leavePolicy.setName("leave");
+        leavePolicy.setId("4");
+        leavePolicy.setLeavePolicyRules(leavePolicyRules);
 
-        leavePolicy.setDescription("you have to request for leave before 15 days.");
+        try {
+            boolean status = leavePolicyDao.create(leavePolicy);
+            Assert.assertEquals(true, status);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        boolean status = leavePolicyDao.create(leavePolicy);
-        Assert.assertEquals(true,status);
+        try {
+            LeavePolicy leavePolicy1 = leavePolicyDao.getById("4");
+            Assert.assertNotNull(leavePolicy1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        boolean status1=leavePolicyDao.delete("1");
-        Assert.assertEquals(true,status1);
-    }*/
+    }
+
 }
--- src/test/java/com/brevitaz/dao/LeavepolicyRuleDaoTest.java
+++ src/test/java/com/brevitaz/dao/LeavepolicyRuleDaoTest.java
@@ -1,4 +1,125 @@
 package com.brevitaz.dao;
 
+import com.brevitaz.model.LeavePolicyRule;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.IOException;
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class LeavepolicyRuleDaoTest {
-}
+    @Autowired
+    LeavePolicyRuleDao leavePolicyRuleDao;
+
+    @Test
+    public void createTest() {
+        LeavePolicyRule leavePolicyRule = new LeavePolicyRule();
+        leavePolicyRule.setId("1");
+        leavePolicyRule.setName("Abc");
+        leavePolicyRule.setDescription("fcgtyuijklmnjbhvg");
+
+        try {
+            boolean status = leavePolicyRuleDao.create(leavePolicyRule);
+            Assert.assertEquals(true, status);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void getAllTest() {
+        LeavePolicyRule leavePolicyRule = new LeavePolicyRule();
+        leavePolicyRule.setId("2");
+        leavePolicyRule.setName("Abc");
+        leavePolicyRule.setDescription("bfhdj");
+
+        try {
+            boolean status = leavePolicyRuleDao.create(leavePolicyRule);
+            Assert.assertEquals(true, status);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            List<LeavePolicyRule> leavePolicyRules = leavePolicyRuleDao.getAll();
+            Assert.assertNotNull(leavePolicyRules);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void updateTest() {
+        LeavePolicyRule leavePolicyRule = new LeavePolicyRule();
+        leavePolicyRule.setId("3");
+        leavePolicyRule.setName("GHI");
+        leavePolicyRule.setDescription("bbnjdnf");
+
+        try {
+            boolean status = leavePolicyRuleDao.create(leavePolicyRule);
+            Assert.assertEquals(true, status);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            leavePolicyRule.setName("dmnfm");
+            boolean status1 = leavePolicyRuleDao.update(leavePolicyRule,"3");
+            Assert.assertEquals(true,status1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void deleteTest() {
+        LeavePolicyRule leavePolicyRule = new LeavePolicyRule();
+        leavePolicyRule.setId("4");
+        leavePolicyRule.setName("Abc");
+        leavePolicyRule.setDescription("kjdfd");
+
+        try {
+            boolean status = leavePolicyRuleDao.create(leavePolicyRule);
+            Assert.assertEquals(true, status);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            boolean status1 = leavePolicyRuleDao.delete("4");
+            Assert.assertEquals(true,status1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void getByIdTest() {
+        LeavePolicyRule leavePolicyRule = new LeavePolicyRule();
+        leavePolicyRule.setId("4");
+        leavePolicyRule.setName("Mno");
+        leavePolicyRule.setDescription("bhjdfjl");
+
+        try {
+            boolean status = leavePolicyRuleDao.create(leavePolicyRule);
+            Assert.assertEquals(true, status);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            LeavePolicyRule leavePolicyRule1  = leavePolicyRuleDao.getById("4");
+            Assert.assertNotNull(leavePolicyRule1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    }
